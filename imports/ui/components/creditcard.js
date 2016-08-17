87cc02af110347ef8d228a8fec43495382c1a365
Paul Savignano <Paul.Savignano@gmail.com>
2016-08-17 16:09:21 -0700

    Added renderCardList method.

--- imports/ui/components/creditcard.js
+++ imports/ui/components/creditcard.js
@@ -14,9 +14,23 @@ export class CreditCard extends Component {
       token: null,
     }
   }
+  renderCardList() {
+    return (
+      <ul className="credit-card-list clearfix">
+        <li><i data-brand="visa" className="fa fa-cc-visa"></i></li>
+        <li><i data-brand="amex" className="fa fa-cc-amex"></i></li>
+        <li><i data-brand="mastercard" className="fa fa-cc-mastercard"></i></li>
+        <li><i data-brand="jcb" className="fa fa-cc-jcb"></i></li>
+        <li><i data-brand="discover" className="fa fa-cc-discover"></i></li>
+        <li><i data-brand="dinersclub" className="fa fa-cc-diners-club"></i></li>
+      </ul>
+    )
+  }
   render() {
     return (
-      <div className="CreditCard">CreditCard</div>
+      <div className="CreditCard">
+        { this.renderCardList() }
+      </div>
     )
   }
 }
